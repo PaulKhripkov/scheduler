24a2005a9134122c0b481c0a7b50870a7fcce185
Paul Khripkov <paul.khripkov@gmail.com>
2020-10-24 12:31:47 +0300

    Replaced main query from hierarchical to recursive to increase speed of search

--- scheduler.sql
+++ scheduler.sql
@@ -30,18 +30,31 @@ BEGIN
     l_months := f_split_by_semicolon(PR_SCHEDULE_MATRIX, 5);
 
     l_sql := '
-        select min(date_list) from (
-            select trunc(to_date(''' || PR_CUR_TIME || ''', ''DD.MM.YY HH24:MI:SS''), ''HH24'') + (rownum-1)*(interval ''15'' minute) date_list
-            from dual
-            connect by level <= 35712
-        )
-        where     to_char(date_list, ''MM'') in (' || l_months || ')
-              and to_char(date_list, ''DD'') in (' || l_monthdays || ') 
-              and to_char(date_list, ''d'') in (' || l_weekdays || ')
-              and to_char(date_list, ''HH24'') in ('  || l_hours || ')
-              and to_char(date_list, ''MI'') in (' || l_quarters || ')
-              and date_list >= to_date(''' || PR_CUR_TIME || ''', ''DD.MM.YY HH24:MI:SS'')';
-    
+        with t(date_list, next_date, stop) as (
+            select to_char(trunc(to_date(''' || PR_CUR_TIME || '''), ''HH24'')) date_list,
+                   to_char(trunc(to_date(''' || PR_CUR_TIME || '''), ''HH24'') + (interval ''15'' minute)) next_date,
+                   0 stop from dual
+        
+            union all
+            
+            select to_char(to_date(date_list) + (interval ''15'' minute)) date_list,
+                   to_char(to_date(date_list) + (interval ''15'' minute)*2) next_date,
+                   case when 
+                            to_char(to_date(next_date), ''MM'') in (' || l_months || ')
+                        and to_char(to_date(next_date), ''DD'') in (' || l_monthdays || ') 
+                        and to_char(to_date(next_date), ''d'') in (' || l_weekdays || ')
+                        and to_char(to_date(next_date), ''HH24'') in ('  || l_hours || ')
+                        and to_char(to_date(next_date), ''MI'') in (' || l_quarters || ')
+                        and to_date(next_date) >= to_date(''' || PR_CUR_TIME || ''')
+                   then 1 else 0 end stop
+            from t
+            where stop = 0
+            )
+        cycle date_list set cycle to 1 default 0
+        select date_list
+        from t
+        where stop = 1';
+
     execute immediate l_sql into l_date;
 
     return l_date;
